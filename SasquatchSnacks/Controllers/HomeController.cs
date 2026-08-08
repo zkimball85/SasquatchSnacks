ecab995a4ae43a9f71f7e1a231068da20351f426
Zachary Kimball <zkimball6859@students.cptc.edu>
2026-08-07 21:46:34 -0700

    Issue#2 initialize asp dot net core10 mvc project zac (#12)
    * Update site branding and remove Privacy links
    
    - Changed site title and navbar brand to "Sasquatch Snacks"
    - Removed "Privacy" link from navbar and footer
    - Simplified footer to show only copyright and site name
    
    * Update Index and remove content from Privacy page
    
    Updated Index.cshtml heading and description to reflect "Welcome To Sasquatch Snacks" and new tagline. Removed ASP.NET Core documentation link. Cleared all content from Privacy.cshtml, including title and placeholder text.
    
    * Refactor HomeController: update action methods
    
    Removed the Privacy action method from HomeController.
    
    * Fix footer text spacing for "Sasquatch Snacks"
    
    Updated footer to add a space between "Sasquatch" and "Snacks", correcting the text to "&copy; 2026 - Sasquatch Snacks".

--- SasquatchSnacks/Controllers/HomeController.cs
+++ SasquatchSnacks/Controllers/HomeController.cs
@@ -11,10 +11,7 @@ namespace SasquatchSnacks.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
