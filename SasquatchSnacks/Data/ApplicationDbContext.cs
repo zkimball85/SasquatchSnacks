3f0e8bd759e3d39417d92d465dda4c9d88a44c9a
Zachary Kimball <zkimball6859@students.cptc.edu>
2026-08-11 19:15:09 -0700

    Add ApplicationDbContext with SQL Server config with EF Core (#16)
    * Add ApplicationDbContext with SQL Server config
    
    Registered ApplicationDbContext in DI using SQL Server and updated .csproj to remove explicit Data\ folder inclusion.
    
    * Refactor ApplicationDbContext constructor to generic
    
    Updated ApplicationDbContext to use DbContextOptions<ApplicationDbContext> in the constructor. Added comments to guide adding DbSet properties for new models.

---
+++ SasquatchSnacks/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SasquatchSnacks.Models;
+
+namespace SasquatchSnacks.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    // We will add DbSet Properties for each of our models here
+    // after we create them in the Models folder. 
+}
