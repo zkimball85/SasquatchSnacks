1d86f16181ea1c51f7f7692fd391f8c4cfcd90e4
Zac Kimball <z.kimball85@gmail.com>
2026-08-11 19:08:16 -0700

    Add ApplicationDbContext with SQL Server config
    Registered ApplicationDbContext in DI using SQL Server and updated .csproj to remove explicit Data\ folder inclusion.

---
+++ SasquatchSnacks/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SasquatchSnacks.Models;
+
+namespace SasquatchSnacks.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
--- SasquatchSnacks/Program.cs
+++ SasquatchSnacks/Program.cs
@@ -1,4 +1,9 @@
+using SasquatchSnacks.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
