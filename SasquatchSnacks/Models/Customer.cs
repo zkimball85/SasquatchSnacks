1facab01d9c8a73eeb2ffdd08bc8b161657983d8
Zachary Kimball <zkimball6859@students.cptc.edu>
2026-08-11 19:49:23 -0700

    Add Customer model and migration for Customers table (#17)
    * Add Customer model and migration for Customers table
    
    Added Customer entity with required properties. Updated ApplicationDbContext and model snapshot. Created EF Core migration and designer for Customers table.
    
    * Update property nullability and remove required modifier
    
    Removed 'required' from Id; made string properties non-nullable with default initialization.

---
+++ SasquatchSnacks/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SasquatchSnacks.Models;
+
+public class Customer
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    [StringLength(100)]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    [StringLength(100)]
+    public string Email { get; set; } = null!;
+}
