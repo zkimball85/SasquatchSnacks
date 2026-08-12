54b93c3f118c92ad3cbb6351fe2eff715b63a887
Zac Kimball <z.kimball85@gmail.com>
2026-08-11 19:48:11 -0700

    Update property nullability and remove required modifier
    Removed 'required' from Id; made string properties non-nullable with default initialization.

--- SasquatchSnacks/Models/Customer.cs
+++ SasquatchSnacks/Models/Customer.cs
@@ -5,17 +5,17 @@ namespace SasquatchSnacks.Models;
 public class Customer
 {
     [Key]
-    public required int Id { get; set; }
+    public int Id { get; set; }
 
     [Required]
     [StringLength(100)]
-    public string? FirstName { get; set; }
+    public string FirstName { get; set; } = null!;
 
     [Required]
     [StringLength(100)]
-    public string? LastName { get; set; }
+    public string LastName { get; set; } = null!;
 
     [Required]
     [StringLength(100)]
-    public string? Email { get; set; }
+    public string Email { get; set; } = null!;
 }
