88f2b8de808783c7fc057814303d790161a7a3b5
Zac Kimball <z.kimball85@gmail.com>
2026-08-11 21:13:58 -0700

    Add e-commerce models and update EF Core context
    Added ConsumableProduct, RentalEquipment, RentalBooking, and VendingLocation entities with validation and relationships. Updated ApplicationDbContext with new DbSets. Created AddECommerceModels migration and updated model snapshot for new tables and relationships.

---
+++ SasquatchSnacks/Models/ConsumableProduct.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SasquatchSnacks.Models;
+
+/// <summary>
+/// Represents a consumable product offered by 
+/// Sasquatch Snacks that can be purchased by customers.
+/// </summary>
+public class ConsumableProduct
+{
+    // The Id is a required field and will be used as the primary key
+    // for the ConsumableProduct table in the database.
+    [Key]
+    public int Id { get; set; }
+
+    // The product name is a required field and will be used to identify the product being sold.
+    [Required]
+    public string ProductName { get; set; } = null!;
+
+    // The product description is a required field and will be used to provide more information about the product being sold.
+    public string Category { get; set; } = null!;
+
+    // The price is a required field and will be used to determine the cost of the product.
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    // The stock quantity is a required field and will be used to determine
+    public int StockQuantity { get; set; }
+}
---
+++ SasquatchSnacks/Models/VendingLocation.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SasquatchSnacks.Models;
+
+/// <summary>
+/// Represents a vending location where Sasquatch Snacks 
+/// automated vending machines are located and their products are available for purchase.
+/// </summary>
+public class VendingLocation
+{
+    // The Id is a required field and will be used as the primary key
+    [Key]
+    public int Id { get; set; }
+
+    // The location name is a required field and will be used to identify
+    // the vending location.
+    [Required]
+    public string LocationName { get; set; } = null!;
+
+    // The address is an optional field and will be used to provide the
+    // physical address of the vending location.
+    public string? Address { get; set; }
+
+    // The operating hours is an optional field and will be used to provide
+    // the hours of operation for the vending location.
+    public string OperatingHours { get; set; } = null!;
+
+    // The isPubliclyAccessible is a required field and will be used to determine
+    // if the vending location is accessible to the public or not.
+    public bool IsPubliclyAccessible { get; set; }
+}
